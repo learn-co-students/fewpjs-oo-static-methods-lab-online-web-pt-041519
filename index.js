8f15768f486bba3bd62cec1b02ba80cd823b3ddc
github username <github email address>
2019-11-24 22:45:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-'-\s]+/g, '');
+  }
+
+  static titleize(str) {
+    return str.toLowerCase().split(' ').map(word => {
+      const noCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+      if(noCap.indexOf(word) !== -1 && str.startsWith(word) !== true) {
+        return word
+      } else {
+        return (word.charAt(0).toUpperCase() + word.slice(1))
+      }
+    }).join(' ')
+  }
+
 }
