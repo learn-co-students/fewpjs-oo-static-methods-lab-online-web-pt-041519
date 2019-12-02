0d6d961178de84eae152711d46548033779b0118
earthctzn <earthctzn1@gmail.com>
2019-12-01 23:50:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-'|\s]+/g, '')
+  }
+
+  static titleize(str) {
+    let newArr = []
+    let words = str.split(' ')
+    let ignoredWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    for (let i = 0; i < words.length; i++) {
+      if ( i === 0 ) {
+          newArr.push( this.capitalize( words[i] ) )
+        } else {
+          if ( ignoredWords.includes( words[i] ) ) {
+            newArr.push( words[i] )
+          } else {
+            newArr.push( this.capitalize( words[i] ) )
+          }
+        }
+      }
+    return newArr.join( " " )
+  }
 }
