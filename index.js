db9248057a924a16265103d707ddd349e9440196
Renee Nordholm <reneenordholm@gmail.com>
2019-12-05 18:57:05 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^0-9a-zA-Z  '-]/g, '')
+  }
+
+  static titleize(string){
+    let splitString = string.split(' ')
+    let newArray = []
+    let ignoredWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    splitString.map( (s) => { 
+      if(s === splitString[0]) {
+        newArray.push(Formatter.capitalize(s))
+      } else if ( ignoredWords.includes(s) ) {
+        newArray.push(s)
+      } else {
+        newArray.push(Formatter.capitalize(s))
+      }
+    })
+    return newArray.join(' ')
+     
+  }
 }
